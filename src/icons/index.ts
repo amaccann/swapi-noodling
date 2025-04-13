8fa167de5fb72cb738d4f97e84d765c50fa6a9a4
Andrew MacCann <andrew.maccann@gmail.com>
2025-04-13 15:52:21 +0100

    Update JEST snapshot

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,5 +1,6 @@
 export {default as ArrowLeftIcon} from './ArrowLeftIcon';
 export {default as PlanetIcon} from './PlanetIcon';
+export {default as PersonIcon} from './PersonIcon';
 export {default as RocketIcon} from './RocketIcon';
 export {default as SortAscIcon} from './SortAscIcon';
 export {default as SortDescIcon} from './SortDescIcon';
